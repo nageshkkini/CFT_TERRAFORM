0c8f87b7bf78ce148d3398b422d09bdde948876e
nageshkkini <89379541+nageshkkini@users.noreply.github.com>
2021-08-23 14:43:48 +0530

    Create main.tf

---
+++ demo1/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  
+  required_version = ">=0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+
+  ami= "ami-00399ec92321828f5"
+  tags= {
+    name = "Nagesh_Terraform"
+}
+}
